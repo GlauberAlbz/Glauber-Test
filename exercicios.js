91a0691360ca4bd864bac54963a687caa42946a5
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-03 20:25:56 -0300

    Exercicios de JS finalizados

---
+++ exercicios.js
@@ -0,0 +1,26 @@
+console.log("Olá mundo");
+
+let var1 = 2;
+let var2 = 3;
+
+console.log(var1+var2);
+
+let num = 4
+
+if (num % 2 == 1){
+    console.log("Número é impar.")
+} else
+    console.log("Número é par.")
+
+let idade = 18
+
+if (idade >= 18){
+    console.log("Você é maior de idade.")
+} else
+    console.log("Você é menor de idade.")
+
+let i
+
+for(i = 1; i < 11; i++) {
+    console.log(i)
+}
