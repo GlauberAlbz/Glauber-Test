a78338be220886d4a179371bead0d35392118055
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-15 21:28:40 -0300

    Exercicio 11 finalizado

---
+++ ex11.js
@@ -0,0 +1,16 @@
+/*
+ Autores: Glauber Almeida B. - Luis Henrique N. C. Pozenato
+    Turma: 2ºA DS               Data: 15/09/2025
+
+    Exercício 11
+    - Crie um objeto “pessoa” com nome, idade, e cidade, depois exiba uma frase com os dados do objeto: “Maria tem 25 anos e mora em São Paulo
+*/
+
+
+let pessoa = {
+    nome: "Maria",
+    idade: 25,
+    cidade: "São Paulo"
+}
+
+console.log(`${pessoa.nome} tem ${pessoa.idade} anos e mora em ${pessoa.cidade}`)
