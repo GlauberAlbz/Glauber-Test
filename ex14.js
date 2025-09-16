7c2fcced991d106f2bc504d07a5601bb9afea57b
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-15 22:00:02 -0300

    Finalização do exercicio 14

---
+++ ex14.js
@@ -0,0 +1,19 @@
+/*
+ Autores: Glauber Almeida B. - Luis Henrique N. C. Pozenato
+    Turma: 2ºA DS               Data: 15/09/2025
+
+    Exercício 14
+    - Calcule o fatorial de um número (ex: 5! = 5 * 4 * 3 * 2 * 1 = 120)
+*/
+
+let num = 5
+let fatorial = 5
+
+
+for (let i = 4; i > 0; i--){
+    fatorial = fatorial * i
+    console.log(fatorial)
+}
+
+
+
