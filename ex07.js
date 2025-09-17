83549254cf74e34b798b27c25b0c57bda77b888a
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-17 20:20:27 -0300

    Resolução Alternativa adicionada

--- ex07.js
+++ ex07.js
@@ -6,9 +6,22 @@
     - Dado um array de números, calcule a soma de todos eles (ex: [1,2,3,4] -> 10)
 */
 
+
 let nums = [1,2,3,4]
 let result
 
 result = nums[0] + nums[1] + nums[2] + nums[3]
 
+
 console.log(result)
+
+/* Código alternativo:
+
+let nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+let result = 0
+
+for(let i = 0; i < 20; i++){
+    result += nums[i]
+}
+
+*/
