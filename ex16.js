3edc4cf82e7a8bae54803362ed02606eaa650da5
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-17 19:52:48 -0300

    Adição do exercicio 16

---
+++ ex16.js
@@ -0,0 +1,8 @@
+/*
+ Autores: Glauber Almeida B. - Luis Henrique N. C. Pozenato
+    Turma: 2ºA DS               Data: 17/09/2025
+
+    Exercício 16
+    - (Bônus desafio ++) Verifique se uma palavra é um palíndromo (lida igual de trás para frente) (ex: “arara”, “radar”)
+*/
+
