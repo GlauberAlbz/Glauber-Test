aae4cc6b792ee73ee40eb2995ea3ce520e54441e
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-19 19:27:12 -0300

    Adição do Exercicio 13

---
+++ ex13.js
@@ -0,0 +1,10 @@
+/*
+    Autores: Glauber Almeida B. - Luis Henrique Nunes C. P.
+    Turma: 2ºA DS               Data: 19/09/2025
+
+    Exercício 13
+    - Escreva um programa que conte de 1 a 100 e:
+      Se for múltiplo de 3, mostre “Batata”
+      Se for múltiplo de 5, mostre “Quente”
+      Se for múltiplo de 3 e 5, mostre “BatataQuente”
+*/
